a99b0da8eea8776b677199cfe51974ee902d0cf9
Monica Crowe <mcrowe9194@gmail.com>
2022-05-07 17:04:08 -0500

    Add project files.

---
+++ BasicClassesExercise1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicClassesExercise1
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+            myCar.Make = "Chevy";
+            myCar.Model = "Equinox";
+            myCar.Year = 2016;
+
+            var dodge = new Car()
+            {
+                Make = "Dodge",
+                Model = "Ram",
+                Year = 2002
+            };
+
+            var chrysler = new Car("Chrysler", "PT Cruiser", 2001);
+
+            var carList = new List<Car>() { myCar, dodge, chrysler };
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+        }
+    }
+}
